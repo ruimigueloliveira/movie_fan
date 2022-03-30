c95a1527ffaa494b7a7b6139a6129c4ab11892e5
Almeida-a <aralmeida99@gmail.com>
2022-03-30 16:05:13 +0100

    OAuth operations file
    Does not contain implementated operations yet, however the steps are planned with todo tags

---
+++ registry/oauth_operations.py
@@ -0,0 +1,38 @@
+from typing import Tuple
+import cryptography
+
+import users_operations as u
+
+SENTINEL = object()
+# Registry entity key pair
+PRIV_KEY: str = "priv_key543"
+PUB_KEY: str = "pub_key546"
+
+
+def authorization_code(username: str, email: str, password: str) -> Tuple[int, str]:
+    # Check credentials
+    status: int = u.check(password, username, email)
+    if status != u.OK:
+        return status, ""
+    # TODO join all strings and sign
+    ...
+
+
+def access_token(auth_code: str, username: str, email: str, password: str) -> Tuple[int, str]:
+    # TODO validate auth_token
+    if not authorization_code(username, email, password)[1] == auth_code:
+        # Return an error status code
+        ...
+    # TODO Re-sign w/ time.time_ns()
+    ...
+
+
+def validate_access_token(access_tkn: str) -> int:
+    # TODO Decrypt w/ pub key in order to obtain the credentials and deadline
+    ...
+    # TODO Check if time has crossed deadline
+    ...
+
+
+if __name__ == '__main__':
+    ...
